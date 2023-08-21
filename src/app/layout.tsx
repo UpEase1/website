ca40f61d29f27bb622eaf96614e78badfef26847
Lance <lance@upease.biz>
2023-08-21 21:27:59 +0530

    Setup config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,9 @@ import { Metadata } from 'next';
 import * as React from 'react';
 
 import '@/styles/globals.css';
-// !STARTERCONF This is for demo purposes, remove @/styles/colors.css import immediately
-import '@/styles/colors.css';
 
 import { siteConfig } from '@/constant/config';
 
-// !STARTERCONF Change these default meta
-// !STARTERCONF Look at @/constant/config to change them
 export const metadata: Metadata = {
   title: {
     default: siteConfig.title,
--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
-  title: 'Next.js + Tailwind CSS + TypeScript Starter',
+  title: 'Upease',
   description:
-    'A starter for Next.js, Tailwind CSS, and TypeScript with Absolute Import, Seo, Link component, pre-configured with Husky',
+    'Data driven solutions to empower educators, engage students and bring intelligence!',
   /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  url: 'https://tsnext-tw.thcl.dev',
+  url: 'https://www.upease.com',
 };
