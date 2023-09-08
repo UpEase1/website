6861481b60a769963ccd535ff3b7db5e3d05d9ee
Lance <lance@upease.biz>
2023-09-08 17:41:32 +0530

    Add navbar

--- src/components/Navbarblack.tsx
+++ src/components/Navbarblack.tsx
@@ -1,23 +1,29 @@
+import { motion, useScroll, useTransform } from 'framer-motion';
 import React from 'react';
 
-
-const NavbarBlack : React.FC = () => {
+const NavbarBlack: React.FC = () => {
+  const targetRef = React.useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+    offset: ['end end', 'end start'],
+  });
+  const opacity = useTransform(scrollYProgress, [0, 0.5], [1, 0]);
   return (
-<nav className="bg-[#454444] bg-opacity-40 lg:w-[30%] mx-auto rounded-full text-lg">
-    <div className="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
-        <a href="#" className="text-gray-800 dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6">UpEase</a>
+    <motion.nav
+      className="mx-auto md:text-lg"
+      ref={targetRef}
+    >
+      <div className="container flex bg-[#454444] w-fit rounded-full items-center justify-center p-6 mx-auto  capitalize text-white">
+        <a href="#" className=" border-b-2 border-blue-500 mx-1.5 sm:mx-6">UpEase</a>
 
-        <a href="#" className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">Products</a>
+        <a href="#" className="border-b-2 border-transparent hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">Products</a>
 
-        <a href="#" className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">pricing</a>
+        <a href="#" className="border-b-2 border-transparent hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">pricing</a>
 
-        <a href="#" className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">Contact Us</a>
+        <a href="#" className="border-b-2 border-transparent hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">Contact Us</a>
 
-        
-
-        
-    </div>
-</nav>
+      </div>
+    </motion.nav>
   );
 };
 
